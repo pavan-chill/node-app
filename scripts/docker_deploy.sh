eb9748ab1c028d71ee2b96e3adc4f874d55c4641
pavan-chill <108312876+pavan-chill@users.noreply.github.com>
2022-08-15 09:18:06 +0530

    Update docker_deploy.sh

--- scripts/docker_deploy.sh
+++ scripts/docker_deploy.sh
@@ -3,4 +3,4 @@
 build_env=$1
 #build_id=$2
 sudo docker build -t cpavan8118/node-app:${build_env} .
-sudo docker run -it -p 3000:3000 cpavan8118/node-app:${build_env} 
+sudo docker run -itd -p 3000:3000 cpavan8118/node-app:${build_env} 
