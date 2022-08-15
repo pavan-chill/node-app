c73b2c0d0a791ce3f3d751a62ef39472800431d0
pavan-chill <pavankumarchillakuru.108@gmail.com>
2022-08-15 03:44:57 +0000

    adding sudo to docker

--- scripts/docker_deploy.sh
+++ scripts/docker_deploy.sh
@@ -2,5 +2,5 @@
 
 build_env=$1
 #build_id=$2
-docker build -t cpavan8118/node-app:${build_env} .
-docker run -it -p 3000:3000 cpavan8118/node-app:${build_env} 
+sudo docker build -t cpavan8118/node-app:${build_env} .
+sudo docker run -it -p 3000:3000 cpavan8118/node-app:${build_env} 
